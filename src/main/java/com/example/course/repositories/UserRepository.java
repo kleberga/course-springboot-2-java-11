3b01ac7d75f90aa1ed9d4825beb69702f7159721
kleberga <kleberga@yahoo.com.br>
2021-11-18 13:54:56 -0300

    Service layer, component registration

--- src/main/java/com/example/course/repositories/UserRepository.java
+++ src/main/java/com/example/course/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.course.entities.User;
 
+
+/* nao preciso colocar a anotation @Repository por que estou extendo o JPA que jah eh componente do spring boot */
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
